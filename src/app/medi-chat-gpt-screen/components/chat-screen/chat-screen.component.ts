eb23b8379feaf7a7ceb58f5884b8073d33e46d8d
Sourabh Srivatsa S R <sourabhsrivatsasr@Sourabhs-MacBook-Air.local>
2024-07-22 23:33:00 +0530

    added all changes

--- src/app/medi-chat-gpt-screen/components/chat-screen/chat-screen.component.ts
+++ src/app/medi-chat-gpt-screen/components/chat-screen/chat-screen.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit, Renderer2 } from '@angular/core';
 import { MediChatGptScreenService } from '../../services/medi-chat-gpt-screen.service';
 
 @Component({
@@ -6,15 +6,20 @@ import { MediChatGptScreenService } from '../../services/medi-chat-gpt-screen.se
 	templateUrl: './chat-screen.component.html',
 	styleUrl: './chat-screen.component.scss'
 })
-export class ChatScreenComponent {
+export class ChatScreenComponent implements AfterViewInit {
+	patientDataAquired: boolean = false;
 	messages: { text: string, sentByMe: boolean }[] = [];
 	newMessage: string = '';
-	patientDataAquired: boolean = false;
-	@ViewChild('chatContainer') private chatContainerRef!: ElementRef;
+	loading = false; // Flag to indicate if loading is happening
 
-	constructor(private renderer: Renderer2, private service: MediChatGptScreenService) {
+	@ViewChild('chatContainer') chatContainer!: ElementRef<HTMLDivElement>;
 
+	constructor(private renderer: Renderer2, private service: MediChatGptScreenService) { }
+
+	ngAfterViewInit() {
+		this.scrollToBottom();
 	}
+
 	sendMessage() {
 		if (this.newMessage.trim() !== '') {
 			this.messages.push({ text: this.newMessage, sentByMe: true });
@@ -29,14 +34,22 @@ export class ChatScreenComponent {
 	}
 
 	receiveMsg() {
+		this.loading = true;
 		this.service.receiveMsg(this.newMessage).subscribe({
 			next: (response) => {
+				this.loading = false;
 				this.messages.push({ text: response.message, sentByMe: false });
 			},
 			error: (err) => {
-
+				this.loading = false;
+				this.messages.push({ text: "Something went wrong!! Please try again after sometime", sentByMe: false });
 			}
 		})
+	}
+	private scrollToBottom() {
+		if (this.chatContainer) {
+			this.chatContainer.nativeElement.scrollTop = this.chatContainer.nativeElement.scrollHeight;
+		}
 	}
 
 	isPatientDataAquired(flag: any) {
